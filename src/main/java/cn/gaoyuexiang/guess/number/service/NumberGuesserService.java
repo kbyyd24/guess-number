9cd8b7a7a5d3a01ac8002e5916ea546bfd77cfa2
gaoyx <melo@gaoyuexiang.cn>
2017-04-10 15:00:41 +0800

    implement buildNumber method

--- src/main/java/cn/gaoyuexiang/guess/number/service/NumberGuesserService.java
+++ src/main/java/cn/gaoyuexiang/guess/number/service/NumberGuesserService.java
@@ -1,11 +1,13 @@
 package cn.gaoyuexiang.guess.number.service;
 
+import java.util.Random;
+
 public class NumberGuesserService {
 
   private int numberX;
 
   public void buildNumberX() {
-
+    this.numberX = new Random().nextInt(100) + 1;
   }
 
   public int getNumberX() {
