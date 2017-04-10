c1eede04c6b3ecbe2acad1f9ec2dde0bd1461f94
gaoyx <melo@gaoyuexiang.cn>
2017-04-10 15:37:02 +0800

    use map to save output msg

--- src/main/java/cn/gaoyuexiang/guess/number/Main.java
+++ src/main/java/cn/gaoyuexiang/guess/number/Main.java
@@ -2,10 +2,16 @@ package cn.gaoyuexiang.guess.number;
 
 import cn.gaoyuexiang.guess.number.service.NumberGuesserService;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    Map<Integer, String> comparatorMap = new HashMap<>();
+    comparatorMap.put(1, "You guessed a bigger number. Please try again!");
+    comparatorMap.put(0, "You guessed right number!");
+    comparatorMap.put(-1, "You guessed a smaller number. Please try again!");
     Scanner consoleScanner = new Scanner(System.in);
     System.out.println("Please input your number:");
     int count = 0;
@@ -15,14 +21,10 @@ public class Main {
       int guessedNumber = consoleScanner.nextInt();
       count++;
       int differ = guesserService.compareGuessNumber(guessedNumber);
+      System.out.println(comparatorMap.get(differ));
       if (differ == 0) {
-        System.out.println("You guess right");
-        System.out.println("You used " + count + " times");
+        System.out.printf("You guessed %d times", count);
         break;
-      } else if (differ > 0) {
-        System.out.println("You guessed a bigger number. Please try again");
-      } else {
-        System.out.println("You guessed a smaller number. Please try again");
       }
     }
   }
