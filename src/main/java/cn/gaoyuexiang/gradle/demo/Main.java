5ddd2e0f6e00120f4a0d0b2d353636015ee6d042
gaoyx <melo@gaoyuexiang.cn>
2017-04-10 11:49:51 +0800

    update main

--- src/main/java/cn/gaoyuexiang/gradle/demo/Main.java
+++ src/main/java/cn/gaoyuexiang/gradle/demo/Main.java
@@ -1,7 +1,10 @@
 package cn.gaoyuexiang.gradle.demo;
 
+import java.util.stream.Stream;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    Stream.of(args).forEach(System.out::println);
   }
 }
