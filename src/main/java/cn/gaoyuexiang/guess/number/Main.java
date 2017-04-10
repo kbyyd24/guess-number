e472e88736c45544d19fc8a7bdf84114affd6664
gaoyx <melo@gaoyuexiang.cn>
2017-04-10 15:05:26 +0800

    update main use scanner get input

--- src/main/java/cn/gaoyuexiang/guess/number/Main.java
+++ src/main/java/cn/gaoyuexiang/guess/number/Main.java
@@ -1,10 +1,20 @@
 package cn.gaoyuexiang.guess.number;
 
-import java.util.stream.Stream;
+import cn.gaoyuexiang.guess.number.service.NumberGuesserService;
+
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
-    Stream.of(args).forEach(System.out::println);
+    Scanner consoleScanner = new Scanner(System.in);
+    System.out.println("Please input your number:");
+    int count = 0;
+    NumberGuesserService guesserService = new NumberGuesserService();
+    guesserService.buildNumberX();
+    while (true) {
+      int guessedNumber = consoleScanner.nextInt();
+      count++;
+
+    }
   }
 }
