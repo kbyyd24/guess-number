ab2c91cf33dcb5329f9f93980d64a6989efcd8df
gaoyx <melo@gaoyuexiang.cn>
2017-04-10 15:52:08 +0800

    use ConsoleViewer to deal console

--- src/main/java/cn/gaoyuexiang/guess/number/Main.java
+++ src/main/java/cn/gaoyuexiang/guess/number/Main.java
@@ -1,38 +1,9 @@
 package cn.gaoyuexiang.guess.number;
 
-import cn.gaoyuexiang.guess.number.service.NumberGuesserService;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import cn.gaoyuexiang.guess.number.view.ConsoleViewer;
 
 public class Main {
   public static void main(String[] args) {
-    Map<Integer, String> comparatorMap = new HashMap<>();
-    comparatorMap.put(1, "You guessed a bigger number. Please try again!");
-    comparatorMap.put(0, "You guessed right number!");
-    comparatorMap.put(-1, "You guessed a smaller number. Please try again!");
-    Scanner consoleScanner = new Scanner(System.in);
-    while (true) {
-      System.out.println("Please input your number:");
-      int count = 0;
-      NumberGuesserService guesserService = new NumberGuesserService();
-      guesserService.buildNumberX();
-      while (true) {
-        int guessedNumber = consoleScanner.nextInt();
-        count++;
-        int differ = guesserService.compareGuessNumber(guessedNumber);
-        System.out.println(comparatorMap.get(differ));
-        if (differ == 0) {
-          System.out.printf("You guessed %d times", count);
-          break;
-        }
-      }
-      System.out.println("Do yout want to try again?(y/N)");
-      if (consoleScanner.hasNext() && consoleScanner.next().equalsIgnoreCase( "y"))
-        continue;
-      break;
-    }
-    consoleScanner.close();
+    new ConsoleViewer().dealConsole();
   }
 }
